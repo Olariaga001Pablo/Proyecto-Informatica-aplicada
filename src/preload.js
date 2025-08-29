bafcffe1e342745cd6d188dd484a1fe694db3555
Olariaga001Pablo <162990497+Olariaga001Pablo@users.noreply.github.com>
2025-08-28 21:03:08 -0300

    Revert "Practica"

--- src/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Exponer APIs seguras al contexto de renderizado
-contextBridge.exposeInMainWorld('electronAPI', {
-    loadPage: (pageName) => ipcRenderer.invoke('load-page', pageName)
-});
